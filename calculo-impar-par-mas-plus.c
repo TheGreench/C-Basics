abd8f70eec8cb0991c2adfc9fe1fadf395d4489a
TheGreench <59140337+TheGreench@users.noreply.github.com>
2021-10-13 21:37:16 -0300

    Update calculo-impar-par-mas-plus.c
    Leer un numero N positivo y luego leer una serie de N numeros
    positivos. Para los numeros que sean primos mostarr el numero
    invertido y los que no lo sean mostar divisores

--- calculo-impar-par-mas-plus.c
+++ calculo-impar-par-mas-plus.c
@@ -2,32 +2,73 @@
 Leer un numero N positivo y luego leer una serie de N numeros
 positivos. Para los numeros que sean primos mostarr el numero
 invertido y los que no lo sean mostar divisores
-
-SIN las funciones
 */
 
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h> // Lib para system("PAUSE");
+
+void leer_valido(int *num) {
+	printf("\nIngrese un numero positivo: ");
+	scanf("%d", num);
+	
+	while (*num < 0) {
+		printf("\n[INGRESO UN NUMERO NEGATIVO] - Ingrese un numero positivo: ");
+		scanf("%d", num);
+	}
+}
+
+bool es_primo(int b) {
+	int i = 2;
+	bool primo = true;
+	
+	while((i <= (b / 2)) && primo) {
+		if((b % i) == 0)
+			primo = false;
+		i++;
+	}
+	return primo;
+}
+
+int invertir_numero(int aux) {
+	int inv;
+ 	
+	inv = aux % 10;
+	
+	while (aux > 9) {
+		aux = (aux / 10);
+		inv = (inv * 10) + (aux % 10);
+	}
+	
+ 	return inv;
+}
+
+void cal_divisores(int f) {
+	int divisor_actual, i;
+
+    printf("Los divisores de %d son: ", f);
+
+	for (i = 1; i <= f ; i++){
+		if (f % i == 0){
+			printf("%d, ",i);
+		}
+	}
+}
 
 void main() {
 	int n, i, a;
 	
 	leer_valido(&n);
 	
-	// fflush(stdin);
-	
 	for (i = 1; i <= n; i++) {
 		leer_valido(&a);
 		
-		if (a % 2 == 0) {
-			printf("%d", invertir_numero(a));
+		if (es_primo(a)){
+			printf("%d \n", invertir_numero(a));
 		}
 		else {
 			cal_divisores(a);
 		}
 	}	
-	
-	system("PAUSE");
+
 	return 0;
 }
