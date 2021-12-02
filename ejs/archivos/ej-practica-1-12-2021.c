184df99d3f9069d42b20a7d75135ceafea9b00ed
TheGreench <59140337+TheGreench@users.noreply.github.com>
2021-12-01 21:12:53 -0300

    Update ej-practica-1-12-2021.c

--- ejs/archivos/ej-practica-1-12-2021.c
+++ ejs/archivos/ej-practica-1-12-2021.c
@@ -27,50 +27,99 @@ La carga de datos, puede reanudarse, y en este caso no se deben perder los datos
 El listado debe ser de todos los datos, encolumnados, y en el orden que se encuentran almacenados.
 */
 
+
 #include <stdio.h>
-#define MAX_ALUMNOS 500
 
 typedef struct{
 	int padron;
 	char ape_nom[30];
 	float nota;
 }t_regAlumno;
-typedef t_regAlumno Alumnos[MAX_ALUMNOS];
 
-void cargar_datos(Alumnos alumno, int *ml) {
-  int carga;
-	int i =0;
+// EJERCICIO 1
+void cargarDatos(FILE *arAlumnos) { // El archivo debe estar abierto en modo escritura
+	t_regAlumno rAlumno;
 
-	printf("Desea ingresar un alumno? con 0 sale: ");
-	scanf("%i", &carga);
+	printf("\nIngrese los datos del alumno:\n");
+	printf(" · Padrón del alumno: ");
+	scanf("%d", &rAlumno.padron);
 
-	while (carga!=0 && i<MAX_ALUMNOS) {
-    printf("Nombre del alumno\n");
-		scanf("%s", alumno[i].ape_nom);
-    fflush(stdin);
-		
-    printf("Padron del alumno\n");
-		scanf("%d",  &alumno[i].padron);
-    fflush(stdin);
-		
-    printf("Nota del alumno\n"  );
-		scanf("%f", &alumno[i].nota);
-    fflush(stdin);
-		
-    printf("Desea ingresar un alumno? con 0 sale: ");
-		scanf("%i", &carga);
-		i++;
+	while ((getchar()) != '\n');
+
+	while(rAlumno.padron != 0) {
+		printf(" · Apellido y nombre del alumno: ");
+		scanf("%30[^\n]s", rAlumno.ape_nom);
+
+		while ((getchar()) != '\n');
+
+		printf(" · Nota: ");
+		scanf("%f", &rAlumno.nota);
+
+		while ((getchar()) != '\n');
+
+		fwrite(&rAlumno, sizeof(rAlumno), 1, arAlumnos);
+
+		printf("\nIngrese los datos del alumno:\n");
+		printf(" · Padrón del alumno: ");
+		scanf("%d", &rAlumno.padron);
+
+		while ((getchar()) != '\n');
 	}
-	*ml = i;
+}
+// FIN EJERCICIO 1
+
+// EJERCICIO 2
+void mostrarDatos(FILE *arAlumnos) {
+	t_regAlumno rAlumno;
+
+	fread(&rAlumno, sizeof(rAlumno), 1, arAlumnos);
+
+	printf("\nPadrón \tApellido y Nombre \t\tNota");
+	while(!feof(arAlumnos)) {
+		printf("\n %d \t%-30s %6.2f", rAlumno.padron, rAlumno.ape_nom, rAlumno.nota);
+		fread(&rAlumno, sizeof(rAlumno), 1, arAlumnos);
+	}
+}
+// FIN EJERCICIO 2
+
+// EJERCICIO 3
+void menuOpciones(FILE *arAlumnos, int opcion) {
+	if(opcion == 1){
+		arAlumnos = fopen("alumnos.dat", "ab");
+		if(arAlumnos == NULL)
+			arAlumnos = fopen("alumnos.dat", "wb");
+		cargarDatos(arAlumnos);
+		fclose(arAlumnos);
+	} else if(opcion == 2) {
+		arAlumnos = fopen("alumnos.dat", "rb");
+		if(arAlumnos == NULL)
+			printf("\n/!\\ Error: no existe el archivo de alumnos.");
+		else {
+			mostrarDatos(arAlumnos);
+			fclose(arAlumnos);
+		}
+	}
 }
 
-int main(void) {
-  Alumnos alumno;
-  int ml;
+void mostrarMenu(FILE *arAlumnos) {
+	int opcion;
 
-  cargar_datos(alumno, &ml);
-
+	printf("\nElija una de las siguientes opciones:");
+	printf("\n1 - Cargar datos de alumnos");
+	printf("\n2 - Listar alumnos");
+	printf("\nOpción elegida: ");
+	scanf("%i", &opcion);
   
+	while((opcion != 1) && (opcion != 2)) {
+		printf("Opción no válida, por favor vuelva a ingresar una opción: ");
+		scanf("%i", &opcion);
+	}
+	menuOpciones(arAlumnos, opcion);
+}
+// FIN EJERCICIO 3
 
-  return 0;
+int main(void){
+	FILE *arAlumnos;
+	mostrarMenu(arAlumnos);
+	return 0;
 }
