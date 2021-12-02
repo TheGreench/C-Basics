e4eab2a2952f198c5b58c94a5bef1ddccdcfa912
TheGreench <59140337+TheGreench@users.noreply.github.com>
2021-12-01 21:33:18 -0300

    Create ej-practica-2-1-12-2021.c

---
+++ ejs/archivos/ej-practica-2-1-12-2021.c
@@ -0,0 +1,49 @@
+/*
+Se tiene un archivo de números telefónicos cuyos registros son de la forma: 
+Característica de localidad (4 dígitos) 
+Característica central (4 dígitos) 
+Número (4 dígitos) 
+
+Dado un número que representa una característica de localidad, listar todos los números telefónicos con dicha característica. 
+
+Suponer los siguientes casos: 
+  a) El archivo no cabe en memoria y está desordenado. 
+  b) El archivo no cabe en memoria y está ordenado según números telefónicos crecientes. 
+*/
+
+#include <stdio.h>
+
+typedef struct{
+  int localidad;
+  int central;
+  int numero;
+} t_nRegistro;
+
+void mostrar_datos(FILE *telRegistros, int localidad) {
+  t_nRegistro nRegistros;
+  int i=0;
+  telRegistros = fopen("telRegistros.dat", "rb");
+
+  if (telRegistros == NULL) 
+    printf("ERR");
+  else {
+    while (!feof(telRegistros)){
+      fread(&nRegistros, sizeof(nRegistros), 1, telRegistros);
+      if (nRegistros.localidad == localidad){
+        printf("%i-%i-%i",nRegistros.localidad, nRegistros.central, nRegistros.numero);
+      }
+    }
+    
+  }
+}
+
+int main(void) {
+  FILE *telRegistros;
+  int localidad;
+
+  printf("Ingrese una caracteristica de localidad (4 digitos): ");
+  scanf("%i", &localidad);
+  mostrar_datos(telRegistros, localidad);
+
+  return 0;
+}
